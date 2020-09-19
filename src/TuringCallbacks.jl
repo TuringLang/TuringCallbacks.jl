98ce2fa7db35bec43666c430e1a0d72fa489c7c3
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2020-09-19 14:42:08 +0100

    changed to using OnlineStats.edges instead of manual impl

--- src/TuringCallbacks.jl
+++ src/TuringCallbacks.jl
@@ -15,55 +15,6 @@ const TBL = TensorBoardLogger
 #########################################
 ### Overloads for `TensorBoardLogger` ###
 #########################################
-
-"""
-    centers_to_edges(centers)
-
-Returns a vector of length `length(centers) + 1`, whose elements represents the edges of the
-bins, rather than the `centers`.
-
-This is useful for converting something like a `OnlineStats.KHist` to a 
-`StatsBase.Histogram` since the former uses the *centers* of the bins while the latter
-requires the *edges* of the bins.
-
-# Examples
-```jldoctest
-julia> using TuringCallbacks: OnlineStats, StatsBase, centers_to_edges
-
-julia> xs = 1.:20;
-
-julia> khist = OnlineStats.fit!(OnlineStats.KHist(10), xs)
-KHist: n=20 | value=(centers = [1.0, 3.5, 6.5, 8.5, 10.5, 12.5, 14.5, 17.0, 19.0, 20.0], counts = [1, 4, 2, 2, 2, 2, 2, 3, 1, 1])
-
-julia> centers, cnts = OnlineStats.value(khist)
-(centers = [1.0, 3.5, 6.5, 8.5, 10.5, 12.5, 14.5, 17.0, 19.0, 20.0], counts = [1, 4, 2, 2, 2, 2, 2, 3, 1, 1])
-
-julia> StatsBase.Histogram(centers_to_edges(centers), cnts, :left, false)
-StatsBase.Histogram{Int64,1,Tuple{Array{Float64,1}}}
-edges:
-  [-0.25, 2.25, 5.0, 7.5, 9.5, 11.5, 13.5, 15.75, 18.0, 19.5, 20.5]
-weights: [1, 4, 2, 2, 2, 2, 2, 3, 1, 1]
-closed: left
-isdensity: false
-```
-"""
-function centers_to_edges(centers)
-    # Find the midpoint between the nearby centers.
-    intermediate = map(2:length(centers)) do i
-        # Pick the left mid-point
-        (centers[i] + centers[i - 1]) / 2
-    end
-    # Left-most point
-    Δ_l = (centers[2] - centers[1]) / 2
-    leftmost = centers[1] - Δ_l
-
-    # Right-most point
-    Δ_r = (centers[end] - centers[end - 1]) / 2
-    rightmost = centers[end] + Δ_r
-
-    return vcat([leftmost], intermediate, [rightmost])
-end
-
 function TensorBoardLogger.preprocess(name, stat::OnlineStat, data)
     return TensorBoardLogger.preprocess(name, value(stat), data)
 end
@@ -78,10 +29,11 @@ end
 
 function TensorBoardLogger.preprocess(name, hist::KHist, data)
     # Creates a NORMALIZED histogram
-    hist_raw = value(hist)
-    edges = centers_to_edges(hist_raw.centers)
-    cnts = hist_raw.counts ./ sum(hist_raw.counts)
-    return TensorBoardLogger.preprocess(name, Histogram(edges, cnts, :left, true), data)
+    edges = OnlineStats.edges(hist)
+    cnts = OnlineStats.counts(hist)
+    return TensorBoardLogger.preprocess(
+        name, Histogram(edges, cnts ./ sum(cnts), :left, true), data
+    )
 end
 
 # Unlike the `preprocess` overload, this allows us to specify if we want to normalize
@@ -89,9 +41,8 @@ function TensorBoardLogger.log_histogram(
     logger::TBLogger, name::AbstractString, hist::OnlineStats.HistogramStat;
     step=nothing, normalize=false
 )
-    hist_raw = value(hist)
-    edges = centers_to_edges(hist_raw.centers)
-    cnts = Float64.(hist_raw.counts)
+    edges = edges(hist)
+    cnts = Float64.(OnlineStats.counts(hist))
     hist = if normalize
         Histogram(edges, cnts ./ sum(cnts), :left, true)
     else
@@ -112,6 +63,8 @@ Wraps a `TensorBoardLogger.TBLogger` to construct a callback to be passed to
 `Turing.sample`.
 
 See also [`make_callback`](@ref).
+
+TODO: add some information about the stats logged.
 
 # Fields
 $(TYPEDFIELDS)
@@ -185,7 +138,7 @@ function make_callback(
                         hist_window = fit!(KHist(window_num_bins), value(buffer))
 
                         @info "$k" stat
-                        @info "$k" hist_window
+                        @info "$k/stat/" hist_window
 
                         # Because the `Distribution` and `Histogram` functionality in
                         # TB is quite crude, we additionally log "later" values to provide
@@ -193,7 +146,7 @@ function make_callback(
                         # TODO: make this, say, 25% of the total number of iterations
                         if iteration > 0.25 * num_samples
                             @info "$k/late" stat
-                            @info "$k/late" hist_window
+                            @info "$k/late/state/" hist_window
                         end
                     end
                 end
