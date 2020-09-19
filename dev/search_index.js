b76e92ab785e1c979817c1c9893a1e242e0a44da
Documenter.jl <documenter@juliadocs.github.io>
2020-09-19 03:56:35 +0000

    build based on 7f2f35e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TuringCallbacks","category":"page"},{"location":"#TuringCallbacks","page":"Home","title":"TuringCallbacks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TuringCallbacks]","category":"page"}]
+}
