df95338dacce46a343e8f5eddc0815a82561540e
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-02-24 15:32:57 +0000

    added a MultiCallback

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,12 @@ using Turing
 using TuringCallbacks
 using TensorBoardLogger, ValueHistories
 
+Base.@kwdef struct CountingCallback
+    count::Ref{Int}=Ref(0)
+end
+
+(c::CountingCallback)(args...; kwargs...) = c.count[] += 1
+
 @testset "TuringCallbacks.jl" begin
     # TODO: Improve.
     @model function demo(x)
@@ -23,19 +29,36 @@ using TensorBoardLogger, ValueHistories
     # Sampling algorithm to use
     alg = NUTS(num_adapts, 0.65)
 
-    # Create the callback
-    callback = TensorBoardCallback(mktempdir())
+    @testset "MultiCallback" begin
+        callback = MultiCallback(CountingCallback(), CountingCallback())
+        chain = sample(model, alg, num_samples, callback=callback)
 
-    # Sample
-    chain = sample(model, alg, num_samples; callback=callback)
+        # Both should have been trigger an equal number of times.
+        counts = map(c -> c.count[], callback.callbacks)
+        @test counts[1] == counts[2]
+        @test counts[1] == num_samples
 
-    # Extract the values.
-    hist = convert(MVHistory, callback.logger)
+        # Add a new one and make sure it's not like the others.
+        callback = TuringCallbacks.push!!(callback, CountingCallback())
+        counts = map(c -> c.count[], callback.callbacks)
+        @test counts[1] == counts[2] != counts[3]
+    end
 
-    # Compare the recorded values to the chain.
-    m_mean = last(last(hist["m/stat/Mean"]))
-    s_mean = last(last(hist["s/stat/Mean"]))
+    @testset "TensorBoardCallback" begin
+        # Create the callback
+        callback = TensorBoardCallback(mktempdir())
 
-    @test m_mean ≈ mean(chain[:m])
-    @test s_mean ≈ mean(chain[:s])
+        # Sample
+        chain = sample(model, alg, num_samples; callback=callback)
+
+        # Extract the values.
+        hist = convert(MVHistory, callback.logger)
+
+        # Compare the recorded values to the chain.
+        m_mean = last(last(hist["m/stat/Mean"]))
+        s_mean = last(last(hist["s/stat/Mean"]))
+
+        @test m_mean ≈ mean(chain[:m])
+        @test s_mean ≈ mean(chain[:s])
+    end
 end
