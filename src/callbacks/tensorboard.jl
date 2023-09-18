fb436224775b0cce6e52bb975e0fb4e15daf3071
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-09-18 21:47:42 +0100

    updated docstring for TensorBoardLogger

--- src/callbacks/tensorboard.jl
+++ src/callbacks/tensorboard.jl
@@ -30,9 +30,22 @@ provided instead of `lg`.
   particular variable and value; expected signature is `filter(varname, value)`.
   If `isnothing` a default-filter constructed from `exclude` and
   `include` will be used.
-- `exclude = nothing`: If non-empty, these variables will not be logged.
-- `include = nothing`: If non-empty, only these variables will be logged.
+- `exclude = String[]`: If non-empty, these variables will not be logged.
+- `include = String[]`: If non-empty, only these variables will be logged.
 - `include_extras::Bool = true`: Include extra statistics from transitions.
+- `extras_include = String[]`: If non-empty, only these extra statistics will be logged.
+- `extras_exclude = String[]`: If non-empty, these extra statistics will not be logged.
+- `extras_filter = nothing`: Filter determining whether or not we should log
+   extra statistics; expected signature is `filter(extra, value)`.
+   If `isnothing` a default-filter constructed from `extras_exclude` and
+   `extras_include` will be used.
+- `include_hyperparams::Bool = true`: Include hyperparameters.
+- `hyperparam_include = String[]`: If non-empty, only these hyperparameters will be logged.
+- `hyperparam_exclude = String[]`: If non-empty, these hyperparameters will not be logged.
+- `hyperparam_filter = nothing`: Filter determining whether or not we should log
+   hyperparameters; expected signature is `filter(hyperparam, value)`.
+   If `isnothing` a default-filter constructed from `hyperparam_exclude` and
+   `hyperparam_include` will be used.
 - `directory::String = nothing`: if specified, will together with `comment` be used to
    define the logging directory.
 - `comment::String = nothing`: if specified, will together with `directory` be used to
